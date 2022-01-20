97e4ceb0002093ff9e8d61bc2cb126e4208c44aa
Ramanan <pramanan3@isye-nagistumbp.local>
2022-01-20 15:00:55 -0500

    initial commit

---
+++ create/hello_world_func.py
@@ -0,0 +1,4 @@
+import os,sys
+
+def hello_world_func(input_name):
+    print("Hello "+str(input_name) + "!")
---
+++ layering/hello-world2.py
@@ -0,0 +1,4 @@
+import os, sys
+from hello_world_func import *
+
+hello_world_func("Alice")
